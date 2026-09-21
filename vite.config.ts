f001b3eb0358770d7fd09c0d7be07cb0a9da5db8
Joyce <joycemasallajorge2006@gmail.com>
2026-09-21 20:50:41 -0300

    correção vercel

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsConfigPaths from "vite-tsconfig-paths";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    TanStackRouterVite(),
+    tanstackStart(),
     react(),
     tsConfigPaths(),
   ],
