f97f9a02aaacd6cd3ce5742050235817ebf6b0cd
Joyce <joycemasallajorge2006@gmail.com>
2026-09-21 21:50:23 -0300

    fix: update vite config for vercel deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,13 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    tanstackStart({
-      srcDirectory: "app"
-    }),
+    tanstackStart({ srcDirectory: "app" }),
+    nitro(),
     react(),
     tsConfigPaths(),
   ],
