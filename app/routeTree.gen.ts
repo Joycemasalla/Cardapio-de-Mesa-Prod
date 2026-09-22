765986813d33f8a99659f24af7c095e3e9c1d2bb
Joyce <joycemasallajorge2006@gmail.com>
2026-09-21 21:08:09 -0300

    correção pro vercel

--- app/routeTree.gen.ts
+++ app/routeTree.gen.ts
@@ -58,12 +58,12 @@ export const routeTree = rootRouteImport
   ._addFileChildren(rootRouteChildren)
   ._addFileTypes<FileRouteTypes>()
 
-import type { getRouter } from './router.tsx'
+import type { createRouter } from './router.tsx'
 import type { startInstance } from './start.ts'
 declare module '@tanstack/react-start' {
   interface Register {
     ssr: true
-    router: Awaited<ReturnType<typeof getRouter>>
+    router: Awaited<ReturnType<typeof createRouter>>
     config: Awaited<ReturnType<typeof startInstance.getOptions>>
   }
 }
